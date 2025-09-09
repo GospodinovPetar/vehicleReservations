faba4152316aebefe5647b5ab38920453e10fabd
Yaren Hasan <yarenhsn@abv.bg>
2025-09-09 15:35:07 +0300

    added a file named forms.py; added a code in settings.py and made some changes here and there.

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Location, Vehicle, Reservation
+from .models import Location, Vehicle, Reservation, User
 from django.contrib.auth.admin import UserAdmin
 
 
