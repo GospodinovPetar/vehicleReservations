f9e84916d02641443cbb0d02534060b60e391ce3
Yaren Hasan <yarenhsn@abv.bg>
2025-09-16 23:43:24 +0300

    made some changes in accounts file and templates file fixing the problems with admin and manager. Making some changes for the admin and manager dashboards

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from .models import CustomUser
-from inventory.models.vehicle import Vehicle
+from inventory.models.vehicle import Vehicle, VehicleType, EngineType, Location
 from inventory.models.reservation import Reservation, ReservationStatus, Location
 from django.contrib.auth import get_user_model
 
@@ -27,28 +27,41 @@ class UserEditForm(forms.ModelForm):
     """
     Admin edit form for users (no password fields).
     """
+
     class Meta:
         model = User
         fields = ["username", "email", "first_name", "last_name", "role", "phone", "is_blocked"]
 
 
 class VehicleForm(forms.ModelForm):
+    car_type = forms.ChoiceField(choices=VehicleType.choices, label="Car Type")
+    engine_type = forms.ChoiceField(choices=EngineType.choices, label="Engine Type")
+
+    available_pickup_locations = forms.ModelMultipleChoiceField(
+        queryset=Location.objects.all(),
+        required=True,
+        widget=forms.CheckboxSelectMultiple,
+        label="Available Pick-up Locations",
+    )
+    available_return_locations = forms.ModelMultipleChoiceField(
+        queryset=Location.objects.all(),
+        required=True,
+        widget=forms.CheckboxSelectMultiple,
+        label="Available Drop-off Locations",
+    )
+
     class Meta:
         model = Vehicle
-        fields = ["name", "car_type", "seats", "price_per_day", "pickup_location", "dropoff_location"]
-
-    car_type = forms.ChoiceField(choices=Vehicle.car_type.choices)  # uses TextChoices
-
-    pickup_location = forms.ModelChoiceField(
-        queryset=Location.objects.all(),
-        required=True,
-        label="Pick-up Location"
-    )
-    dropoff_location = forms.ModelChoiceField(
-        queryset=Location.objects.all(),
-        required=True,
-        label="Drop-off Location"
-    )
+        fields = [
+            "name",
+            "car_type",
+            "engine_type",
+            "seats",
+            "unlimited_seats",
+            "price_per_day",
+            "available_pickup_locations",
+            "available_return_locations",
+        ]
 
 
 class ReservationStatusForm(forms.ModelForm):
